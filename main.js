1e16cc65ae1269f42bc7c201731628e3c6242b84
laura <106088132+lalinha07@users.noreply.github.com>
2023-12-07 07:36:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function tocaSom(idElementoAudio){
+document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas = document.querySelectorAll("tecla");
+
+let contador = 0;
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom (idAudio)
+    }
+    contador = contador + 1;
+    console.log(contador);
+
+    listaDeTeclas.onkeydown = function(evento){
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            listaDeTeclas.classlist.add('ativa')
+        }
+    }
+    listaDeTeclas.onkeyup = function(){
+        listaDeTeclas.classList.remuve('ativa')
+    }
+
+}
